58163035af8f1d3d50294f6826b9884a24d3c94a
Void User <amshulha@ualberta.ca>
2026-09-18 17:23:40 -0600

    beginning derivative, work on MRA

--- derivative.py
+++ derivative.py
@@ -1,4 +1,6 @@
 import math
+import matplotlib.pyplot as plt
+import numpy as np
 
 n = 30
 h = 1
@@ -9,10 +11,25 @@ for i in range(n):
     h = 0.5*h
     y = (math.sin(x+h)-math.sin(x))/h
     error = abs(math.cos(x)-y)
-    print(error)
     if error > error_max:
         emax = error
         imax = i
 
-print(imax,emax)
+#print(imax,emax)
 
+
+def f(x,y):
+    return x**2+y**2
+
+def linear_j(f,x0,y0,step_size):
+    h = step_size
+    f_0 = f(x0,y0)
+    delta_x = (f(x0+h,y0) - f_0 ) / h
+    delta_y = (f(x0,y0+h) - f_0 ) / h
+    delta = np.array([delta_x,delta_y])
+    return delta
+
+
+x = np.linspace(0,1,100)
+X,Y=np.meshgrid(x,x)
+print(linear_j(f,X,Y,1e-3))
---
+++ multiresolution.py
@@ -0,0 +1,98 @@
+import numpy as np
+from numpy.polynomial import Chebyshev
+import matplotlib.pyplot as plt
+import scipy.stats as stat
+from scipy.integrate import quad
+from scipy.stats import gaussian_kde
+from scipy.interpolate import CubicSpline
+
+
+def multi_res(f,j,n):
+    """Given a function f, dilate it by 2**j and translate it 2**(-j)*n direction"""
+    return lambda x : np.sqrt(2**j)*f(2**j*x-2**(-j)*n)
+
+
+expr = 'x**6'
+def f(x):
+    return eval(expr)
+
+plt.plot(np.linspace(-1,1,100),f(np.linspace(-1,1,100)))
+plt.title('Signal '+ expr)
+plt.grid()
+plt.show()
+
+fig, ax = plt.subplots(3,3,figsize=(8,8))
+for j,ax in enumerate(ax.flat):
+    for n in range(-3,3):
+        x = np.linspace(-2**(8-j),2**(8-j),100)
+        y = multi_res(lambda x : stat.norm.pdf(x),j-3,n)
+        noise = np.random.normal(x)
+        #fs = f(x) + noise
+        #z = np.convolve(fs,y(x),mode='same')
+        z = lambda x : f(x)*y(x)
+        l = np.linalg.norm(z(x))
+        #ax.plot(x,y(x)+((-1)**n)*0.25*n,label=f'translate  {-n} ')
+        ax.plot(x,z(x)/l+((-1)**n)*0.25*n,label=f'translate  {-n} ')
+        ax.set_xlim(-2**(8-j),2**(8-j))
+        ax.set_title(f"Scaling at level {j-4} and translates",fontsize=9)
+        ax.tick_params('both',labelsize=5)
+        ax.grid(True)
+#        ax.legend()
+plt.tight_layout()
+plt.show()
+
+
+coefs = []
+k=0
+basis_funcs = []
+for j in range(9):
+
+    for n in range(-3,3):
+        x = np.linspace(-2**(8-j),2**(8-j),100)
+        y = multi_res(lambda x : stat.norm.pdf(x),j-3,n)
+        basis_funcs.append(y)
+        #noise = np.random.normal(x)
+        fs = f(x)
+        #z = np.convolve(fs,y(x),mode='same')
+        z = lambda x : f(x)*y(x) 
+        z, _ = quad(z,-2,2)
+        point = k
+        coefs.append(np.array([point,z]))
+        k += 1
+
+coefs = np.array(coefs)
+#interp = np.polynomial.Chebyshev(coefs[:,1],domain=[-1,1])
+points = coefs[:,0]
+n = len(coefs)
+normalized_points = 2*points / (n-1) - 1
+
+
+phi = np.vstack([func(normalized_points) / np.linalg.norm(func(normalized_points)) for func in basis_funcs ])
+construc = coefs[:,1]@phi
+
+
+interp = CubicSpline(normalized_points,coefs[:,1])
+interp_2 = Chebyshev(coefs[:,1])
+interp_3 = gaussian_kde(coefs[:,1],bw_method='scott')
+plt.scatter(normalized_points,coefs[:,1],label=f'translate')
+plt.plot(normalized_points,interp(normalized_points),label='Cubic Spline')
+plt.plot(normalized_points,interp_2(normalized_points), label = 'Chebyshev')
+plt.plot(normalized_points,interp_3(normalized_points), label = 'Gaussian KDE')
+plt.tick_params('both',labelsize=5)
+plt.grid(True)
+plt.ylim(-1,15)
+ax.legend(loc="upper left")
+plt.tight_layout()
+plt.show()
+
+plt.step(normalized_points,coefs[:,1])
+plt.fill_between(normalized_points,coefs[:,1],step='mid',alpha=0.3)
+plt.title('Step plot of coefficients')
+plt.show()
+
+
+plt.plot(normalized_points,construc,label='reconstructed curve')
+plt.plot(normalized_points,f(normalized_points),label='original curve')
+plt.legend()
+plt.grid(True)
+plt.show()
