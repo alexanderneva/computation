1cde0a985a3c0f3a8a5a05fdc29b367be6c46e13
Void User <amshulha@ualberta.ca>
2026-09-22 00:10:09 -0600

    lutka-volterra in matlab

--- matlab/script.m
+++ matlab/script.m
@@ -36,6 +36,45 @@ end
 print -djpg ../fields/matlab_figure.jpg;
 hold off;
 
+
+% Lutka-volterra
+t = y = linspace(0,7,50);
+
+[tt,yy] = meshgrid (t,y);
+
+%tz = vectorField(tt,yy);
+fs = 't*(y-2)'
+gs = 'y*(1-t)'
+f = @(t,y) [eval(fs)];
+g = @(t,y) [eval(gs)];
+
+dx = f(tt,yy);
+dy = g(tt,yy);
+
+L = hypot(dx, dy);
+L(L==0) = 1;
+dx = dx ./ L;
+dy = dy ./ L;
+
+figure;
+quiver (tt,yy,dx,dy, 1, 'k');
+hold on;
+grid on;
+xlabel('t'); ylabel('y');
+title(strcat('Vector Field u1 =',fs,' u2 = ',gs));
+
+sol = @(t, z) [f(z(1), z(2)); g(z(1),z(2))];
+tspan = [0 3];
+
+initial_conditions = [0.25 1; 0.5, 1; 1, 1; 1, 0.25; 0, 0.25; 0, 0.5; 1, 2];
+for k= 1:rows(initial_conditions);
+  [t, z] = ode45(sol, tspan, initial_conditions(k, :));
+  plot(z(:,1), z(:,2), 'LineWidth',1);
+  plot(z(1,1), z(1,2),'ro', 'MarkerFaceColor','r');
+end
+
+print -djpg ../fields/matlab_figure_lutka.jpg;
+hold off;
 %f_2 = @(t,y) [1 - t.^2 - y.^2];
 %
 %z = f_2 (tt, yy);
