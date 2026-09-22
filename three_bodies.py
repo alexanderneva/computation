76cd33794b1828528a538af6b783702e078c22b0
Void User <amshulha@ualberta.ca>
2026-09-21 21:34:02 -0600

    somewhat working two and three bodies

--- three_bodies.py
+++ three_bodies.py
@@ -4,44 +4,48 @@ import matplotlib.pyplot as plt
 G = 6.6743e-11
 
 
-def f(t,s_1,s_2,s_3,s_1p=0,s_2p=0,s_3p=0):
+def f(t,s_1,s_2,s_3,s_1p=np.zeros(3),s_2p=np.zeros(3),s_3p=np.zeros(3)):
     y_1 = s_1
     y_2 = s_2
     y_3 = s_3
-    y_4 = s_1p
-    y_5 = s_2p
-    y_6 = s_3p
+    v_1 = s_1p
+    v_2 = s_2p
+    v_3 = s_3p
     x_0 = np.array([y_1,y_2,y_3])
-    r_12 = np.linalg.norm(s_1-s_2)
-    r_23 = np.linalg.norm(s_2-s_3)
-    r_13 = np.linalg.norm(s_1-s_3)
-    y_1_p = y_4
-    y_2_p = y_5
-    y_3_p = y_6
-    y_4_p = -G*(m_2*r_13**2 + m_3*r_12**2) / (r_12*r_13)**2
-    y_5_p = -G*(m_1*r_23**2 + m_3*r_12**2) / (r_12*r_23)**2
-    y_6_p = -G*(m_3*r_12**2 + m_1*r_23**2) / (r_23*r_12)**2
-    v_1_p = np.array([y_1_p,y_2_p,y_3_p,y_4_p,y_5_p,y_6_p])
-    #x_1 = np.array([y_1,y_2,y_3])+t*v_1_p[0:3]+t**2*v_1_p[3:]
-    x_1 = x_0 + t*v_1_p[0:3] + 0.5*t**2*v_1_p[3:]
+    r_12 = s_1 - s_2
+    r_12n = np.linalg.norm(r_12)
+    r_23 = s_2 - s_3
+    r_23n = np.linalg.norm(r_23)
+    r_13 = s_1 - s_3
+    r_13n = np.linalg.norm(s_1-s_3)
+    v_1 += t*G*(m_2*r_13n**2 + m_3*r_12n**2) / (r_12n*r_13n)**2 * (r_12 + r_13)
+    v_2 += t*G*(m_1*r_23n**2 + m_3*r_12n**2) / (r_12n*r_23n)**2 * (r_12 + r_23)
+    v_3 += t*G*(m_3*r_12n**2 + m_1*r_23n**2) / (r_13n*r_12n)**2 * (r_23 + r_12)
+    y_1_p = t*v_1
+    y_2_p = t*v_2
+    y_3_p = t*v_3
 
 
-    return x_1
+    return y_1_p,y_2_p,y_3_p,v_1,v_2,v_3
 
-s_1 = np.random.normal(loc=(2,2,2),size=(1,3))
-s_2 = np.random.normal(loc=(2,-2,-2),size=(1,3))
-s_3 = np.random.normal(loc=(-2,2,2),size=(1,3))
+s_1 = np.random.normal(loc=(2,2,2))
+s_2 = np.random.normal(loc=(2,-2,-2))
+s_3 = np.random.normal(loc=(-2,2,2))
+v_1 = np.zeros(3)
+v_2 = np.zeros(3)
+v_3 = np.zeros(3)
 s_1_i = s_1.copy()
 s_2_i = s_2.copy()
 s_3_i = s_3.copy()
 print(f"s_1 {s_1}\n s_2 {s_2} \n s_3 {s_3}")
 
-total_step = 100
-time = np.arange(1,total_step)
 
-m_1 = 300e3
-m_2 = 5e4
+m_1 = 3e5
+m_2 = 5e5
 m_3 = 7e5
+
+total_step = 500
+time = np.arange(1,total_step)
 length = np.size(time)
 print(length)
 points = np.zeros(shape=(length+1,3,3))
@@ -50,8 +54,8 @@ points[0,...] += pos
 
 
 for t in time:
-    h = 0.25
-    s_1,s_2,s_3 = f(h,s_1,s_2,s_3,1,1,1)
+    h = 0.0001 
+    s_1,s_2,s_3,v_1,v_2,v_3 = f(h,s_1,s_2,s_3,v_1,v_2,v_3)
     pos = np.vstack([s_1,s_2,s_3])
     points[t,...] += pos
 
@@ -66,8 +70,11 @@ print(np.shape(x_1))
 ax.plot(x_1[:,0],x_1[:,1],x_1[:,2],label=f'first mass {m_1}')
 ax.plot(x_2[:,0],x_2[:,1],x_2[:,2],label=f'second mass {m_2}')
 ax.plot(x_3[:,0],x_3[:,1],x_3[:,2],label=f'third mass {m_3}')
-ax.scatter(s_1_i[:,0],s_1_i[:,1],s_1_i[:,2],label=f'first initial point',color='red')
-ax.scatter(s_2_i[:,0],s_2_i[:,1],s_2_i[:,2],label=f'second initial point',color='red')
-ax.scatter(s_3_i[:,0],s_3_i[:,1],s_3_i[:,2],label=f'third initial point',color='red')
+ax.scatter(s_1_i[0],s_1_i[1],s_1_i[2],label=f'first initial point',color='red')
+ax.scatter(s_2_i[0],s_2_i[1],s_2_i[2],label=f'second initial point',color='red')
+ax.scatter(s_3_i[0],s_3_i[1],s_3_i[2],label=f'third initial point',color='red')
+ax.scatter(x_1[-1,0],x_1[-1,1],x_1[-1,2],label=f'first terminal point',color='purple')
+ax.scatter(x_2[-1,0],x_2[-1,1],x_2[-1,2],label=f'second terminal point',color='purple')
+ax.scatter(x_3[-1,0],x_3[-1,1],x_3[-1,2],label=f'third terminal point',color='purple')
 plt.legend()
 plt.show()
--- two_bodies_plane.py
+++ two_bodies_plane.py
@@ -12,10 +12,19 @@ def f(t,s_1,s_2,s_1p=np.zeros(2),s_2p=np.zeros(2),m_1=1.,m_2=1.):
     y_4 = s_2p
     #x_0 = np.vstack([y_1,y_2,y_3,y_4])
     r_12 = np.linalg.norm(s_1-s_2)
-    y_1_p = y_1 - t*(y_3 @ s_1-s_2) / r_12
-    y_2_p = y_2 + t*(y_4 @ s_2-s_1) / r_12
-    y_3_p = -0.5*t*(s_1-s_2) * G *  m_2   / r_12**2
-    y_4_p = 0.5*t*(s_2 -s_1) * G * m_1 / r_12**2
+    if r_12 <= 0.005:
+        """Elastic collision with jitter"""
+        y_3_p = y_3 - 2*m_2/(m_1+m_2) * np.dot(y_3-y_4,s_1 - s_2) / r_12**2 * (s_1-s_2) + np.random.normal(loc=y_3)
+        y_4_p = y_4 - 2*m_1/(m_1+m_2) * np.dot(y_4-y_3,s_2 - s_1) / r_12**2 * (s_2-s_1) + np.random.normal(loc=y_4)
+
+        y_1_p = y_1 + t*y_3_p
+        y_2_p = y_2 + t*y_4_p
+        return y_1_p,y_2_p,y_3_p,y_4_p
+    y_3_p = y_3 - t*(s_1-s_2) * G *  m_2   / r_12**3
+    y_4_p = y_4 + t*(s_1-s_2) * G * m_1 / r_12**3
+
+    y_1_p = y_1 + t*y_3_p
+    y_2_p = y_2 + t*y_4_p
     #v_1_p = np.vstack([y_1_p,y_2_p,y_3_p,y_4_p])
     #x_1 = np.array([y_1,y_2,y_3])+t*v_1_p[0:3]+t**2*v_1_p[3:]
     return y_1_p,y_2_p,y_3_p,y_4_p
@@ -25,12 +34,14 @@ s_2 = np.random.normal(loc=(-2,2))
 s_1_i = s_1.copy()
 s_2_i = s_2.copy()
 print(f"s_1 {s_1}\n s_2 {s_2}")
+print(f"s_1 {s_1.shape}\n s_2 {s_2.shape}")
 
-total_step = 100
+
+total_step = 1000
 time = np.arange(1,total_step)
 
-m_1 = 300e3
-m_2 = 5e4
+m_1 = 1e10
+m_2 = 1e10
 length = np.size(time)
 print(length)
 points = np.zeros(shape=(length+1,2,2))
@@ -45,16 +56,17 @@ points[0,...] += pos
 #print(v_1)
 #print(v_2)
 #
-v_1=np.array([1,1])
-v_2=np.array([-1,1])
-#v_1 = np.zeros(2)
-#v_2 = np.zeros(2)
+#v_1=np.random.normal(loc=s_1)
+#print("v_1 shape", v_1.shape)
+#v_2=np.random.normal(loc=s_2)
+v_1 = np.zeros(2)
+v_2 = np.zeros(2)
 for t in time:
-    h = 0.25
+    h = 0.1
     #s_1,s_2 = f(h,s_1,s_2,s_3)
     s_1,s_2,v_1,v_2 = f(h,s_1,s_2,v_1,v_2,m_1,m_2)
     pos = np.vstack([s_1,s_2])
-    points[t,...] += pos
+    points[t,...] += pos.copy()
 #
 #
 time = np.arange(0,total_step)
@@ -67,5 +79,7 @@ ax.plot(x_1[:,0],x_1[:,1],label=f'first mass {m_1}')
 ax.plot(x_2[:,0],x_2[:,1],label=f'second mass {m_2}')
 ax.scatter(s_1_i[0],s_1_i[1],label=f'first initial point',color='red')
 ax.scatter(s_2_i[0],s_2_i[1],label=f'second initial point',color='red')
+ax.scatter(x_1[-1,0],x_1[-1,1],label=f'first terminal point',color='purple')
+ax.scatter(x_2[-1,0],x_2[-1,1],label=f'second terminal point',color='purple')
 plt.legend()
 plt.show()
