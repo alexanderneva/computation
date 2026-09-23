c169b9e7bbbb09ff2c1a05299fd76e64d53dbc5c
Void User <amshulha@ualberta.ca>
2026-09-22 22:19:55 -0600

    3D sirs vector field, particular phase soln

--- vector_field.py
+++ vector_field.py
@@ -43,8 +43,8 @@ funcs = [f_1,f_2,f_3,f_4,f_5,f_6,f_7,f_8]
 exprs = [expr_0,expr_1,expr_2,expr_3,expr_4,expr_5,expr_6,expr_7]
 
 
-test = np.linspace(-10,10,25)
-I, J = np.meshgrid(test,test)
+#test = np.linspace(-10,10,25)
+#I, J = np.meshgrid(test,test)
 #step = 0.1
 #fig, ax = plt.subplots(3,2)
 #U,V = f_9(I,I,J)
@@ -53,30 +53,30 @@ I, J = np.meshgrid(test,test)
 #V /= length
 #
 
-sol = solve_ivp(f_9,np.array([0,2]),np.array([1,-1]),vectorized=True)
-t = sol.t
-y = sol.y
+#sol = solve_ivp(f_9,np.array([0,2]),np.array([1,-1]),vectorized=True)
+#t = sol.t
+#y = sol.y
 
-plt.figure()
-U,V = f_9(I,[J,J])
-#q = ax[index].quiver(i,j,i+0.5,0.5*func(i,j)+j)
-length = np.sqrt(U**2 + V**2)
-q = plt.quiver(I,J,U / length,V / length,scale=25,angles='xy')
-plt.quiverkey(q, X=0.3,Y=1.1,U=2,label='Length of 2',labelpos='E')
-for points in np.array([[0.1,1.2],[-1.1,0.1],[-2,-11.2]]):
-    jac = jacobian(lambda x_bar : f_9(0,x_bar),points)
-    print(jac.df)
-    vals = np.linalg.eigvals(jac.df)
-    print("value", vals.astype(float))
-    sol = solve_ivp(f_9,np.array([0,5]),points)
-    sol_2 = solve_ivp(f_9,np.array([0,5]),vals.astype(float))
-    t = sol.t / np.linalg.norm(t)
-    y = sol.y / np.linalg.norm(y)
-    y_2 = sol_2.y 
-    y_2 /= np.linalg.norm(y_2)
-    plt.plot(y[0],y[1])
-    #plt.plot(y_2[0],y_2[1])
-plt.show()
+#plt.figure()
+#U,V = f_9(I,[J,J])
+##q = ax[index].quiver(i,j,i+0.5,0.5*func(i,j)+j)
+#length = np.sqrt(U**2 + V**2)
+#q = plt.quiver(I,J,U / length,V / length,scale=25,angles='xy')
+#plt.quiverkey(q, X=0.3,Y=1.1,U=2,label='Length of 2',labelpos='E')
+#for points in np.array([[0.1,1.2],[-1.1,0.1],[-2,-11.2]]):
+#    jac = jacobian(lambda x_bar : f_9(0,x_bar),points)
+##    print(jac.df)
+#    vals = np.linalg.eigvals(jac.df)
+##    print("value", vals.astype(float))
+#    sol = solve_ivp(f_9,np.array([0,5]),points)
+#    sol_2 = solve_ivp(f_9,np.array([0,5]),vals.astype(float))
+#    t = sol.t / np.linalg.norm(t)
+#    y = sol.y / np.linalg.norm(y)
+#    y_2 = sol_2.y 
+#    y_2 /= np.linalg.norm(y_2)
+#    plt.plot(y[0],y[1])
+#    #plt.plot(y_2[0],y_2[1])
+#plt.show()
 
 
 
@@ -102,4 +102,56 @@ plt.show()
 ##    plt.savefig(f'fields/field{index}.jpg')
 #
 ##    plt.show()
-        
+
+
+from ode_system import sirs_bv
+
+deltas = np.linspace(0.25,0.75,3)
+gammas = np.linspace(0.25,0.75,3)
+lambdas = np.linspace(0.25,0.75,3)
+bs = np.linspace(0,1,4)
+b = bs[1]
+d = 1 - b
+delta = deltas[1]
+lamb = lambdas[1]
+gamma = gammas[1]
+K = 1.5
+
+test = np.linspace(0,1,25)
+x = np.meshgrid(test,test,test)
+
+points = sirs_bv(b,d,K,delta,lamb,gamma,test,x)
+U = points[:,0]
+V = points[:,1]
+W = points[:,2]
+
+sol = solve_ivp(lambda t,x : sirs_bv(b,d,K,delta,lamb,gamma,t,x),(0,1),np.array([0.9,0.1,0.]),t_eval=test)
+t = sol.t
+y = sol.y
+#print(y.shape)
+#plt.plot(t,y[0,:],label='susc')
+#plt.plot(t,y[1,:],label='inf')
+#plt.plot(t,y[2,:],label='rec')
+#plt.legend()
+#plt.tight_layout()
+#plt.show()
+#length = np.sqrt(U**2 + V**2 + W**2)
+ax = plt.figure().add_subplot(projection='3d')
+for b in bs:
+    for delta in deltas:
+        for gamma in gammas:
+            for lamb in lambdas:
+                d = 1 - b
+                points = sirs_bv(b,d,K,delta,lamb,gamma,test,x)
+                u = points[:,0]
+                v = points[:,1]
+                w = points[:,2]
+                ax.quiver(test,test,test,u,v,w ,length=0.1,normalize=True)
+ax.scatter(y[0,:],y[1,:],y[2,:])
+ax.set_xlabel("s")
+ax.set_ylabel("i")
+ax.set_zlabel("r")
+ax.set_zlim([np.min(y[2,:]),np.max(y[2,:])])
+plt.tight_layout()
+plt.show()
+#ax.quiverkey(q, X=0.3,Y=1.1,U=1,label='Length of 2',labelpos='E')
